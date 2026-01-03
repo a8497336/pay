22cd506e5d6f27e1f88142f1859a79bebadc0a55
a8497338 <1779840057@qq.com>
2026-01-03 19:26:44 +0800

    test

--- backend/test.js
+++ backend/test.js
@@ -1,3 +1,3 @@
 import liantuofuPayService from './src/services/liantuofuPayService.js'
 
-liantuofuPayService.queryPaymentStatus('D1767426037352XU4R')
+liantuofuPayService.queryPaymentStatus('D1767428842405E1O1')
